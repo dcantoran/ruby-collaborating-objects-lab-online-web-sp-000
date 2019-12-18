6743458e979b07b7f249ae2a386bf1c92fabfd08
David Cantoran <davidcantoran36@gmail.com>
2019-12-18 23:11:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,12 +15,13 @@ class Song
     @@all
   end 
   
-  def self.new_by_filename
-    
+  def self.new_by_filename(file_name)
+    file = self.new(file_name)
+    save
   end 
   
   def artist_name=(name)
-    self.name.find_or_create_by_name(name)
+    # self.name.find_or_create_by_name(name)
   end 
   
 end 
