3ed5229f3ac297bb9f467822d4d897645d9e98b7
David Cantoran <davidcantoran36@gmail.com>
2019-12-18 22:54:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,11 @@ class Artist
   
   def self.find_or_create_by_name(artist_name)
     # @@all.find {|artist| artist.name == artist_name} || Artist.new(artist_name).save
-      self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+      if self.all.detect {|artist| artist.name == name} 
+        self.all.detect {|artist| artist.name == name} 
+      else 
+        Artist.new(artist_name).save
+      end 
   end 
   
 end 
