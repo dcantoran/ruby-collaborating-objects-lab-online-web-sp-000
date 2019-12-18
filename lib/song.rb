98a2159f7809188fc38872cb827b27f5d53c589f
David Cantoran <davidcantoran36@gmail.com>
2019-12-18 23:24:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   def self.new_by_filename(file_name)
     file = self.new(file_name)
     artist_name = file.name.split("-")[0].strip
-    file.artist = artist_name
+    file.artist_name = artist
     file
     # binding.pry
   end 
