073992004421fd11b4e348b9889029815d97a815
David Cantoran <davidcantoran36@gmail.com>
2019-12-18 23:47:04 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,7 @@ class MP3Importer
   end 
   
   def files 
-    Dir.entries(@path).uniq.select {|file| file.include?(".mp3")}
+    Dir.entries(@path).select {|file| file.include?(".mp3")}
     # binding.pry
   end 
   
